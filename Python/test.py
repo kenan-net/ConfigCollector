9e21a7ae043610d6447dfbbec38340a825d5deed
Kenan Biberkic <kenan.net@gmail.com>
2018-06-30 22:22:16 +0200

    device_class_test: adding file which should be used as an example and as a file which test the functionality of the Device class.
    Signed-off-by: Kenan Biberkic <kenan.net@gmail.com>

--- Python/test.py
+++ Python/test.py
@@ -1,18 +1,32 @@
 #!/usr/bin/python3
 import logging
-from Device import Device
+import src.Device
 
 logging.basicConfig(format='%(name)s - %(levelname)s:%(message)s', level=logging.INFO)
+
+
+def process_command_output (error, message):
+	if error:
+		print("Executing command failed.")
+		print("Error message: {0}".format(message))
+	else:
+		print("Executing command OK.")
+		print("Command output: {0}".format(message))
+	return
 
 
 def main():
 	# Create a new device. When creating a new device, provide a device IP address, username and password which will be
 	# used to establish an SSH connection
-	dev = Device("192.168.0.19", "pi", "raspberry")
+	dev = src.Device.Device("192.168.0.19", "pi", "raspberry")
 	if dev.is_connected():
-		print("Executing commands")
-		dev.execute_command("ls")
-		dev.execute_command("pwd")
+		print("Connected to device. Now executing commands...")
+		(error, message) = dev.execute_command("pwd")
+		process_command_output(error, message)
+
+		(error, message) = dev.execute_command("ls")
+		process_command_output(error, message)
+
 	else:
 		print("Not connected to the device")
 
